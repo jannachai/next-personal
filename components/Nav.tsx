022884f496e597ccfb79cfb128447f8bb689d887
Wannachai Burke <wannachai.burke@water.co.nz>
2023-03-21 00:37:20 +1300

    feat: added hover colours

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,46 +3,53 @@ import React from 'react';
 import { useRouter } from 'next/router';
 
 function Nav() {
-    const router = useRouter();
-    console.log(router.pathname) 
-    return (
-        <div className="navigation-body">
-            <ul>
-                <li className={router.pathname === "/" ? "active" : ""}><Link href="/">HOME</Link></li>
-                <li className={router.pathname === "/about" ? "active" : ""}><Link href="/about">ABOUT</Link></li>
-                <li className={router.pathname === "/resume" ? "active" : ""}><Link href="/resume">RESUME</Link></li>
-                <li className={router.pathname === "/contact" ? "active" : ""}><Link href="/contact">CONTACT</Link></li>
-            </ul>
-            <style jsx>
-                {`
-                    .navigation-body {                    
-                        font-size: 12px;
-                        list-style-type: none;
-                        position: fixed;
-                        right: 2.0833vw;
-                        top: 30px;
-                        text-align: right;
-                        line-height: 18px;
-                        font-family: 'Nunito';
-                        z-index: 10000;
-                    }
-                
-                    a, a:visited, a:active {
-                        color: inherit;
-                        text-decoration: none;
-                    }
-                    
-                    li.active {
-                        font-weight: bold;
-                    }
+  const router = useRouter();
+  console.log(router.pathname);
+  return (
+    <div className="navigation-body">
+      <ul>
+        <li className={router.pathname === '/' ? 'active' : ''}>
+          <Link href="/"><span>HOME</span></Link>
+        </li>
+        <li className={router.pathname === '/about' ? 'active' : ''}>
+          <Link href="/about"><span>ABOUT</span></Link>
+        </li>
+        <li className={router.pathname === '/resume' ? 'active' : ''}>
+          <Link href="/resume"><span>RESUME</span></Link>
+        </li>
+        <li className={router.pathname === '/contact' ? 'active' : ''}>
+          <Link href="/contact"><span>CONTACT</span></Link>
+        </li>
+      </ul>
+      <style jsx>
+        {`
+          .navigation-body {
+            font-size: 12px;
+            list-style-type: none;
+            position: fixed;
+            right: 2.0833vw;
+            top: 30px;
+            text-align: right;
+            line-height: 18px;
+            font-family: 'Nunito';
+            z-index: 10000;
+          }
 
-                    ul {
-                        list-style: none;
-                    }
-                `}
-            </style>
-        </div>
-    )
+          .navigation-body span:hover {
+            color: #09e6c4;
+          }
+
+          li.active {
+            font-weight: bold;
+          }
+
+          ul {
+            list-style: none;
+          }
+        `}
+      </style>
+    </div>
+  );
 }
 
 export default Nav;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,22 @@
-import Head from 'next/head'
-import { createClient } from 'contentful'
-import Link from 'next/link'
-import Layout from '@/components/Layout'
+import Head from 'next/head';
+import { createClient } from 'contentful';
+import Link from 'next/link';
+import Layout from '@/components/Layout';
 
 export async function getStaticProps() {
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID ?? '',
     accessToken: process.env.CONTENTFUL_ACCESS_KEY ?? '',
-  })
+  });
 
-  const res = await client.getEntries({ content_type: 'photographyPost' })
+  const res = await client.getEntries({ content_type: 'photographyPost' });
 
   return {
     props: {
-      photographyPosts: res.items
-    }
-  }
+      photographyPosts: res.items,
+    },
+  };
 }
-
 
 export default function Home({ photographyPosts }: any) {
   return (
@@ -29,31 +28,33 @@ export default function Home({ photographyPosts }: any) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <div className='title-list'>
+        <div className="title-list">
           {photographyPosts.map((post: any) => (
-            <Link href={`/photographyPost/${post.fields.slug}`} key={post.fields.title}>
-              {post.fields.title}<br/>
+            <Link
+              href={`/photographyPost/${post.fields.slug}`}
+              key={post.fields.title}
+            >
+              <span>{post.fields.title}</span>
+              <br />
             </Link>
           ))}
         </div>
       </Layout>
       <style jsx>
-        {
-          `
-            .title-list {
-              font-family: 'Antonio';
-              font-size: 6vw;
-              text-decoration: underline;
-              text-align: center;
+        {`
+          .title-list {
+            font-family: 'Antonio';
+            font-size: 6vw;
+            text-align: center;
+            text-decoration: underline;
+          }
 
-              a, a:visited {
-                  color: inherit; 
-                  text-decoration: none;
-              }
-            }
-          `
-        }
+          .title-list span:hover {
+            color: #09e6c4;
+            text-decoration: underline;
+          }
+        `}
       </style>
     </>
-  )
+  );
 }
